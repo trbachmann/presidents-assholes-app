e4ef35f57a329b5ce3e9b4a3d6b11e9674c206a9
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-26 09:34:55 -0700

    Add isLoadingReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { presidentsReducer } from './presidentsReducer';
+import { isLoadingReducer } from './isLoadingReducer';
 
 export const rootReducer = combineReducers({
   presidents: presidentsReducer,
+  isLoading: isLoadingReducer,
 });
