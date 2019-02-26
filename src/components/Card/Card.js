07616241b655508b9882efee93a11660ee685bc4
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-26 10:11:09 -0700

    Add Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Card = (props) => {
+  const { 
+    number, 
+    president, 
+    birth_year, 
+    death_year, 
+    took_office, 
+    left_office, 
+    party 
+  } = props.president;
+
+  return(
+    <div className='Card'>
+      <p>Number: {number}</p>
+      <p>President: {president}</p>
+      <p>Birth Year: {birth_year}</p>
+      <p>Death Year: {death_year}</p>
+      <p>Took Office: {took_office}</p>
+      <p>Left Office: {left_office}</p>
+      <p>Party: {party}</p>
+    </div>
+  );
+}
