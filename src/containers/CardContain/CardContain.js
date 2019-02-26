275bb516487b2f9fa7d613c5b559a190d8b8c890
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-26 11:46:42 -0700

    Update CardContain to display Cards based on currentParty

--- src/containers/CardContain/CardContain.js
+++ src/containers/CardContain/CardContain.js
@@ -4,19 +4,32 @@ import { Card } from '../../components/Card/Card';
 import './CardContain.css';
 
 export const CardContain = (props) => {
-  const presidentCards = props.presidents.map(president => {
-    return <Card president={president} key={president.number}/>
-  });
+  const { presidents, currentParty } = props;
+  let presidentsToDisplay = [];
+
+  if (currentParty === 'All') {
+    presidentsToDisplay = presidents.map(currPresident => {
+      return <Card president={currPresident} key={currPresident.number} />
+    });
+  } else {
+    presidentsToDisplay = presidents.reduce( (acc, currPresident) => {
+      if (currPresident.party === currentParty) {
+        acc = [acc, <Card president={currPresident} key={currPresident.number} />]
+      }
+      return acc;
+    }, []);
+  }
   
   return(
     <div className='CardContain'>
-      { presidentCards }
+      { presidentsToDisplay }
     </div>
   );
 }
 
 export const mapStateToProps = (state) => ({
-  presidents: state.presidents
+  presidents: state.presidents,
+  currentParty: state.currentParty
 });
 
 export default connect(mapStateToProps)(CardContain);
