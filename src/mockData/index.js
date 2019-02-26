5771c39698cf4e8d6d5b580e5df5a8a4a8a03868
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-26 10:21:18 -0700

    Add mockData for testing

---
+++ src/mockData/index.js
@@ -0,0 +1,19 @@
+export const mockPresidents = [
+  { number: 1, president: 'George Washington', birth_year: 1732, death_year: 1799, took_office: '1789-04-30', left_office: '1797-03-04', party: 'No Party' },
+  { number: 2, president: 'John Adams', birth_year: 1735, death_year: 1826, took_office: '1797-03-04', left_office: '1801-03-04', party: 'Federalist' },
+  { number: 3, president: 'Thomas Jefferson', birth_year: 1743, death_year: 1826, took_office: '1801-03-04', left_office: '1809-03-04', party: 'Democratic-Republican' },
+  { number: 4, president: 'James Madison', birth_year: 1751, death_year: 1836, took_office: '1809-03-04', left_office: '1817-03-04', party: 'Democratic-Republican' },
+  { number: 5, president: 'James Monroe', birth_year: 1758, death_year: 1831, took_office: '1817-03-04', left_office: '1825-03-04', party: 'Democratic-Republican' },
+  { number: 6, president: 'John Quincy Adams', birth_year: 1767, death_year: 1848, took_office: '1825-03-04', left_office: '1829-03-04', party: 'Democratic-Republican' },
+  { number: 7, president: 'Andrew Jackson', birth_year: 1767, death_year: 1845, took_office: '1829-03-04', left_office: '1837-03-04', party: 'Democratic' }
+];
+
+export const MockPresident = { 
+  number: 1, 
+  president: 'George Washington', 
+  birth_year: 1732, 
+  death_year: 1799, 
+  took_office: '1789-04-30', 
+  left_office: '1797-03-04', 
+  party: 'No Party' 
+};
