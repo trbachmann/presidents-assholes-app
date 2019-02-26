79b3305f1a63011c3ec672f777eda24ca5eefea0
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-26 09:32:57 -0700

    Update presidentsReducer

--- src/reducers/presidentsReducer.js
+++ src/reducers/presidentsReducer.js
@@ -1,6 +1,8 @@
 export const presidentsReducer = (state = [], action) => {
   switch (action.type) {
+    case 'SET_PRESIDENTS':
+      return action.presidents;
     default:
-    return state;
+      return state;
   }
 }
